cbae688f1e073dd0208545df392fbe660a843558
phurbasherpa13 <13phurbasherpa@gmail.com>
2026-03-01 03:11:42 +0545

    feat: exectueation of the base64url for secure

--- foundation.py
+++ foundation.py
@@ -7,4 +7,9 @@ class SecureDataExchange:
     def __init__(self, data=None):
         self.raw_data = data
 
-    pass 
+    def encode_base64url_safe(self, data_dict):
+        """Simulates creating a JWT-like payload (Base64URL)."""
+        json_str = json.dumps(data_dict, separators=(',', ':'))
+        b64_bytes = base64.b64encode(json_str.encode('utf-8'))
+        return b64_bytes.decode('utf-8').replace('+', '-').replace('/', '_').rstrip('=')
+
